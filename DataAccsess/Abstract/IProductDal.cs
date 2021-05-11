d760e47519704daaa1b49107c0e7be5eefbf19a1
cem bayat <cembayat34@outlook.com>
2021-05-11 23:02:59 +0300

    Proje dosyası ekle.

---
+++ DataAccsess/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccsess.Abstract
+{
+    public interface IProductDal //I -> Interface, Product -> Hangi Tabloya karsilik geldigi, Dal -> Hangi katmana karsilik geldigi
+    {                            //Dal = Data accsess layer
+                                 //burada product ile ilgili veritabaninda yapacagim opersayonlari iceren interface. (Operasyon = bunu ekle, sunu sil, bunu getir, bunu listele vb..) 
+
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
